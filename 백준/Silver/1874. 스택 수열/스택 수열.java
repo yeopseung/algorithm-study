a13d48fe68c427745ae4bc08ca8470dd276d7d63
yeopseung <86239380+yeopseung@users.noreply.github.com>
2023-07-25 00:56:25 +0900

    [Silver II] Title: 스택 수열, Time: 1000 ms, Memory: 112656 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        int n, num, start = 1, top = 1;
+
+        StringBuffer sb = new StringBuffer();
+        Stack<Integer> stack = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for(int i=0; i<n ; i++)
+        {
+            num = sc.nextInt();
+
+            if(num >= top)
+            {
+                for(int j=start; j<=num; j++)
+                {
+                    stack.push(j);
+                    sb.append("+\n");
+
+
+                }
+                stack.pop();
+                sb.append("-\n");
+                if(!stack.isEmpty())
+                    top = stack.peek();
+//                printStack(stack);
+                if(start <= num) {
+                    start = num + 1;
+                }
+            }
+            else
+            {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+//    public static void printStack(Stack<Integer> stack)
+//    {
+//        for(int i=0; i<stack.size(); i++)
+//        {
+//            System.out.println(stack.get(i));
+//        }
+//    }
+}
