750453ae152393b78fb695042c5e42d272d245cf
yeopseung <86239380+yeopseung@users.noreply.github.com>
2023-07-29 01:36:45 +0900

    [Gold IV] Title: X와 K, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/1322. X와 K/X와 K.java
@@ -0,0 +1,40 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+        long X, Y, K, k;
+
+
+        st = new StringTokenizer(br.readLine());
+        X = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        Y = 0;
+        k = 0;
+        for(int i=0; i< 65;i++)
+        {
+            if(((X >> i) & 1L) == 1)
+                continue;
+
+            if(((K >> k) & 1L) == 1)
+            {
+                Y = Y | (1L << i);
+            }
+
+            k++;
+        }
+
+        System.out.println(Y);
+    }
+}
