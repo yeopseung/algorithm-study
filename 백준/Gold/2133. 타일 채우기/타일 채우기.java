8f1bcf1769c4bccfbe5174a5bda91988f50547e6
yeopseung <86239380+yeopseung@users.noreply.github.com>
2023-08-06 19:00:13 +0900

    [Gold IV] Title: 타일 채우기, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int[] list = new int[31];
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        Arrays.fill(list,-1);
+        System.out.println(dp(N));
+    }
+
+    static int dp(int num)
+    {
+        int result, sum =0;
+
+        //홀수의 경우 가짓수가 없기 때문에 0 반환
+        if((num%2) != 0)
+            return 0;
+
+        //초기값 설정
+        if(num == 2)
+            return 3;
+        if(num == 4)
+            return 11;
+
+        if(list[num] != -1)
+            return list[num];
+
+
+
+        sum = calc(num-4);
+
+
+
+        result = (dp(num-2) * dp(2)) +  2 *sum +2;
+        list[num] = result;
+        return result;
+
+    }
+
+    static int calc(int num)
+    {
+        int result =0;
+        while(num > 0)
+        {
+            result += dp(num);
+            num -= 2;
+        }
+        return result;
+    }
+}
