7a64b05b40590d6dc6e15c5551fdce8a7d2d5311
yeopseung <86239380+yeopseung@users.noreply.github.com>
2023-10-01 17:03:04 +0900

    [Gold IV] Title: 오큰수, Time: 2300 ms, Memory: 324992 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main
+{
+    public static void main(String[] args) {
+        int N;
+        int[] arr;
+
+
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        Stack<Integer> stack = new Stack<>();
+
+        N = sc.nextInt();
+        arr = new int[N];
+
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+
+        stack.push(0);
+        for (int i = 1; i < N; i++)
+        {
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i])
+            {
+                arr[stack.pop()] = arr[i];
+            }
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty())
+        {
+            arr[stack.pop()] = -1;
+        }
+
+
+
+        for(int i=0; i<N;i++)
+        {
+           sb.append(arr[i]+" ");
+        }
+        System.out.println(sb);
+    }
+}
