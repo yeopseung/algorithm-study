f2bf683e80c3487a8055c569b24eca51baa3634c
yeopseung <86239380+yeopseung@users.noreply.github.com>
2024-01-04 18:08:55 +0900

    [level 2] Title: 전화번호 목록, Time: 307.78 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -8,7 +8,8 @@ class Solution {
         
         for(int i=0; i<phone_book.length-1; i++){
             if(phone_book[i+1].startsWith(phone_book[i])){
-                return false;
+                answer = false;
+                break;
             }
         }
         return answer;
