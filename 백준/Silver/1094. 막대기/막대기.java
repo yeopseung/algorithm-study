ec41d0156a3ab1eb222a2d5d453c42ed4baf94bb
yeopseung <86239380+yeopseung@users.noreply.github.com>
2024-01-22 22:59:11 +0900

    [Silver V] Title: 막대기, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int X, stick, sum, cnt;
+
+        X = sc.nextInt();
+        cnt = 1;
+        stick = 64;
+        sum = 64;
+
+        while(true)
+        {
+            stick = stick >> 1;
+
+            if(sum == X)
+                break;
+
+            if((sum - stick) >= X)
+                sum -= stick;
+            else
+                cnt++;
+        }
+
+        System.out.println(cnt);
+
+    }
+}
