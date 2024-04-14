26f645825528445b4b710eb9ffa547b5dfc36877
yeopseung <86239380+yeopseung@users.noreply.github.com>
2024-04-14 14:37:17 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,23 +2,23 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-       
-        int sum = brown + yellow;   
-        int height = 1;  
+        int sum = brown + yellow;
+        int width = 0;
+        int height = 0;
         
-        while(height <= (sum/2)){
-            if(sum % height == 0){
-                int width = sum / height;
-                int b = (width*2) + (height-2) * 2; 
+        for (int i = 1; i <= Math.sqrt(sum); i++) {
+            if (sum % i == 0) {
+                width = sum / i;
+                height = i;
                 
-                if(b == brown){
+                int b = (width + height) * 2 - 4;
+                
+                if (b == brown) {
                     answer[0] = width;
                     answer[1] = height;
                     break;
                 }
             }
-             
-            height++;
         }
         
         return answer;
