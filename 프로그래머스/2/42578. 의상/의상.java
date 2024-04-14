7b06ff673fa46f6a106249336e1ff5597169268c
yeopseung <86239380+yeopseung@users.noreply.github.com>
2024-04-14 17:19:15 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -7,23 +7,13 @@ class Solution {
         HashMap<String, Integer> map = new HashMap<>();
         for(String[] clothe: clothes){
             String category = clothe[1];
-            
-            if(!map.containsKey(category)){
-                map.put(category, 0);
-            }
-            
-            map.put(category, map.get(category) + 1);
+            map.put(category, map.getOrDefault(category, 0) + 1);
         }
         
-        Set<String> categories = map.keySet();
-        for(String category: categories){
-            int num = map.get(category);
-            
-            answer *= num + 1;
+        for(int clothesNum: map.values()){
+            answer *= clothesNum + 1;
         }
         
-        answer--;
-        
-        return answer;
+        return answer - 1;
     }
 }
