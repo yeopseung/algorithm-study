e17279466d88c76e6886a8424bee52b1bc75c6c7
yeopseung <86239380+yeopseung@users.noreply.github.com>
2024-04-17 15:44:18 +0900

    [level 3] Title: 가장 먼 노드, Time: 55.08 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -5,11 +5,11 @@ class Solution {
         int answer = 0;
         
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i=0; i<=n ; i++){
+        for(int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
         
-        for(int[] e: edge){
+        for(int[] e : edge) {
             int x = e[0];
             int y = e[1];
             
@@ -17,47 +17,32 @@ class Solution {
             graph.get(y).add(x);
         }
         
-        Integer[] distance = new Integer[n+1];
-        Arrays.fill(distance, 0);
+        int[] distance = new int[n + 1];
+        Arrays.fill(distance, -1); // 최단 거리를 -1로 초기화
         
-        boolean[] visited = new boolean[n+1];
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(1, 0));
-        visited[1] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        distance[1] = 0;
         
-        while(!queue.isEmpty()){
-            Node cur = queue.poll();
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
             
-            ArrayList<Integer> next = graph.get(cur.n);
-            
-            distance[cur.n] = Math.max(distance[cur.n], cur.cnt);
-            
-            for(int i=0; i<next.size(); i++){
-                if(!visited[next.get(i)]){
-                    queue.add(new Node(next.get(i), cur.cnt+1));
-                    visited[next.get(i)] = true;
+            for(int next : graph.get(cur)) {
+                if(distance[next] == -1) { // 방문하지 않은 노드라면
+                    queue.add(next);
+                    distance[next] = distance[cur] + 1; // 현재 노드까지의 거리 + 1로 갱신
                 }
             }
         }
         
-        Arrays.sort(distance, Collections.reverseOrder());
-        int max = distance[0];
-        for(int d: distance){
-            if(max == d){
+        Arrays.sort(distance);
+        int maxDistance = distance[distance.length - 1]; // 최대 거리
+        for(int d : distance) {
+            if(d == maxDistance) {
                 answer++;
             }
         } 
         
-        
         return answer;
-    }
-}
-
-class Node{
-    int n, cnt;
-    
-    Node(int n, int cnt){
-        this.n = n;
-        this.cnt = cnt;
     }
 }
